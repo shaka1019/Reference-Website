c787babced216865f59615a9f82a1ce065e5ceb7
ShakaCon <126812100+shaka1019@users.noreply.github.com>
2023-03-07 17:38:09 +0000

    Completed 7.1

---
+++ 7.1-arrays-intro.js
@@ -0,0 +1,25 @@
+function example() {
+  const topStudents = ["Christian", "Kristian", "Shaka", "Jaime", "AJ"];
+  var name = topStudents[2];
+  console.log(name);
+  topStudents[0] = "Mauricio";
+  console.log(topStudents);
+
+}
+
+function problem1() {
+  var travelList = ["Brazil", "Egypt", "Japan", "India", "Ghana"];
+  var place = travelList[2];
+  alert(place) 
+}
+
+
+function problem2(){
+  var numList = ["2", "3", "5", "7", "11"];
+  var num = numList[0];
+  console.log(num);
+  var num1 = numList[2];
+  console.log(num1);
+  var num2 = numList[4];
+  console.log(num2);
+}
